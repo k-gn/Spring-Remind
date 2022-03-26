62374de05dfa74f3d3d46220a218da4aad0b2c5c
k-gn <rlarbska97@gmail.com>
2022-03-26 17:35:38 +0900

    no message

--- spring-basic/fc_1/spring-filter/src/main/java/com/example/filter/SpringFilterApplication.java
+++ spring-basic/fc_1/spring-filter/src/main/java/com/example/filter/SpringFilterApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
-@ServletComponentScan
+@ServletComponentScan // @WebFilter 어노테이션을 읽는다.
 public class SpringFilterApplication {
 
     public static void main(String[] args) {
---
+++ spring-basic/fc_1/spring-interceptor/src/main/java/com/example/interceptor/SpringInterceptorApplication.java
@@ -0,0 +1,13 @@
+package com.example.interceptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInterceptorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringInterceptorApplication.class, args);
+    }
+
+}
