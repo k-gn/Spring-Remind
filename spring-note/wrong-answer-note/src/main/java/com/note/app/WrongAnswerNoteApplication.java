2ba927e9320c245da23f63e1f2c4aeb19cf4bb5a
k-gn <rlarbska97@gmail.com>
2023-02-25 22:09:09 +0900

    chore: project init

---
+++ spring-note/wrong-answer-note/src/main/java/com/note/app/WrongAnswerNoteApplication.java
@@ -0,0 +1,13 @@
+package com.note.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrongAnswerNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WrongAnswerNoteApplication.class, args);
+	}
+
+}
---
+++ spring-note/wrong-answer-note/src/test/java/com/note/app/WrongAnswerNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.note.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrongAnswerNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
