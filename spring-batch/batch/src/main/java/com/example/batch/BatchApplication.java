d344b4aa5f8dae3b0958b831eee0408625e23a22
k-gn <rlarbska97@gmail.com>
2023-03-30 10:39:38 +0900

    feat: spring batch test

--- spring-batch/batch/src/main/java/com/example/batch/BatchApplication.java
+++ spring-batch/batch/src/main/java/com/example/batch/BatchApplication.java
@@ -1,8 +1,11 @@
 package com.example.batch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 보통 실시간 처리가 어려운 대용량 데이터나 대규모 데이터일 경우 배치를 활용한다.
+@EnableBatchProcessing // 배치기능 활성화
 @SpringBootApplication
 public class BatchApplication {
 
---
+++ spring-batch/batch/src/main/java/com/example/batch/job/JobConfig.java
@@ -0,0 +1,50 @@
+package com.example.batch.job;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Configuration
+@RequiredArgsConstructor
+public class JobConfig {
+
+	private final JobBuilderFactory jobBuilderFactory;
+	private final StepBuilderFactory stepBuilderFactory;
+
+	@Bean
+	public Job simpleJob() {
+		return jobBuilderFactory
+			.get("simple-job")
+			.start(simpleStep())
+			.build();
+	}
+           
+	@Bean
+	public Step simpleStep() {
+		return this.stepBuilderFactory
+			.get("simple-step")
+			.tasklet(simpleTasklet())
+			.build();
+	}
+
+	@Bean
+	public Tasklet simpleTasklet() {
+		return (stepContribution, chunkContext) -> {
+			for (int i = 10; i > 0; i--) {
+				TimeUnit.SECONDS.sleep(1);
+				System.out.println(i);
+			}
+			return RepeatStatus.FINISHED;
+		};
+	}
+}
