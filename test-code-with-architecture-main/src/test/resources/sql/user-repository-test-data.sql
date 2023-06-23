36a9e8ac91f77579998dcac0f3412ad65fb8a5b3
k-gn <rlarbska97@gmail.com>
2023-06-23 18:49:36 +0900

    chore: test note init

---
+++ test-code-with-architecture-main/src/test/resources/sql/user-repository-test-data.sql
@@ -0,0 +1,2 @@
+insert into `users`(`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values(1, 'rlarbska97@gmail.com', 'Gyul', 'Seoul', 'aaaaaaa-aaaa-aaaa-aaaaaaaaaaaaa', 'ACTIVE', 0);
