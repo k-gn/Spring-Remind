c5885472322015ee8c58abff6a20b936dc8fe40e
k-gn <rlarbska97@gmail.com>
2023-06-30 19:05:33 +0900

    chore: init

---
+++ point-management/src/main/java/com/app/pointmanagement/PointManagementApplication.java
@@ -0,0 +1,13 @@
+package com.app.pointmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointManagementApplication.class, args);
+	}
+
+}
---
+++ point-management/src/test/java/com/app/pointmanagement/PointManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.pointmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
